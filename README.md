f96e0ec6ed05effc4b186b0962dff9ee001722bb
ObiEuan <geuan@modelo.edu.mx>
2023-05-27 02:24:04 -0600

    added function to remember chat context

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Este es un bot en python que integra la API de twitch y la de chatGPT desarrollado para autoaprendizaje
+
+necesitarás instalar la biblioteca twitchio. 
+pip install twitchio
+necesitarás la biblioteca openai para interactuar con la API de ChatGPT. 
+pip install openai
--- obiAI.py
+++ obiAI.py
@@ -14,6 +14,7 @@ openai.api_key = OPENAI_API_KEY  # Clave de API de OpenAI
 class Bot(commands.Bot):
     def __init__(self):
         super().__init__(token=TWITCH_BOT_TOKEN, prefix='!', initial_channels=[channel for channel in channels])
+        self.chat_history = {}
 
     async def event_ready(self):
         for channel in channels:
@@ -31,13 +32,23 @@ class Bot(commands.Bot):
         if message.author and message.author.name.lower() == bot_nick.lower():
             return
 
+        # Obtener el nombre del canal
+        channel = message.channel.name
+
+        # Verificar si el canal está en el historial del chat
+        if channel not in self.chat_history:
+            self.chat_history[channel] = []
+
+        # Agregar el mensaje actual al historial del chat
+        self.chat_history[channel].append(message.content)
+
         # Verificar si el mensaje contiene el prefijo "@obiai"
         if f'@{bot_nick.lower()}' in message.content.lower():
             # Eliminar el prefijo del mensaje para obtener el texto de entrada para ChatGPT
             input_text = message.content.lower().replace(f'@{bot_nick.lower()}', '').strip()
 
             # Obtener respuesta de ChatGPT
-            response = self.get_chatgpt_response(input_text)
+            response = self.get_chatgpt_response(channel, input_text)
 
             # Enviar la respuesta al chat
             await message.channel.send(f'@{message.author.name}, {response}')
@@ -46,11 +57,17 @@ class Bot(commands.Bot):
     async def saludo(self, ctx):
         await ctx.send(f'Hola {ctx.author.name}!')
 
-    def get_chatgpt_response(self, message):
-        # Llamar a la API de ChatGPT
+    def get_chatgpt_response(self, channel, message):
+        # Obtener el historial del chat para el canal actual
+        chat_history = self.chat_history[channel]
+
+        # Combinar el historial del chat con el mensaje actual como contexto
+        chat_context = '\n'.join(chat_history + [message])
+
+        # Llamar a la API de ChatGPT con el contexto del chat
         response = openai.Completion.create(
             engine='text-davinci-003',
-            prompt=message,
+            prompt=chat_context,
             max_tokens=50,
             n=1,
             stop=None,
@@ -62,6 +79,17 @@ class Bot(commands.Bot):
 
         # Obtener la respuesta generada por ChatGPT
         reply = response.choices[0].text.strip()
+
+        # Agregar la respuesta al historial del chat
+        chat_history.append(reply)
+
+        # Limitar el historial del chat a un número máximo de mensajes
+        max_history = 10
+        if len(chat_history) > max_history:
+            chat_history = chat_history[-max_history:]
+
+        # Actualizar el historial del chat para el canal actual
+        self.chat_history[channel] = chat_history
 
         return reply
 
